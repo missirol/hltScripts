747a13c412e2952a91fdbdb731cbe67b3b30479f
Marino Missiroli <marino.missiroli@cern.ch>
2026-03-31 12:42:29 +0200

    updated rucio/rucio_addrule_example.sh

--- rucio/rucio_addrule_example.sh
+++ rucio/rucio_addrule_example.sh
@@ -6,10 +6,10 @@ datasets=(
 #  /QCD_Bin-PT-15to7000_Par-PT-flat2022_TuneCP5_13p6TeV_pythia8/Run3Winter25Digi-NoPU_142X_mcRun3_2025_realistic_v9-v3/GEN-SIM-RAW
 #  /QCD_Bin-PT-15to7000_Par-PT-flat2022_TuneCP5_13p6TeV_pythia8/Run3Winter25Digi-EpsilonPU_142X_mcRun3_2025_realistic_v9-v4/GEN-SIM-RAW
 #  /QCD_Bin-PT-15to7000_Par-PT-flat2022_TuneCP5_13p6TeV_pythia8/Run3Winter25Digi-FlatPU0to120_142X_mcRun3_2025_realistic_v9-v4/GEN-SIM-RAW
+#  /TT_TuneCP5_13p6TeV_powheg-pythia8/Run3Winter25Digi-142X_mcRun3_2025_realistic_v7-v2/GEN-SIM-RAW
 
-/store/data/Run2026A/L1Scouting/L1SCOUT/v1/000/401/733/00000/81f5a1c5-a8d2-41bd-a9d8-54959c778d9f.root
-/store/data/Run2026A/L1ScoutingSelection/L1SCOUT/v1/000/401/733/00000/52356b7e-e20e-4afb-bde6-6674e2b8b94f.root
-
+  /store/data/Run2026B/L1Scouting/L1SCOUT/v1/000/402/144/00000/af07ed8c-d45e-41ad-81c7-8c32478c40be.root
+  /store/data/Run2026B/L1ScoutingSelection/L1SCOUT/v1/000/402/144/00000/56a86b0d-c4a0-4ffc-84c5-158494ec8ecc.root
 )
 
 # rucio list-rules --account $RUCIO_ACCOUNT | grep ...
@@ -21,7 +21,7 @@ for dataset in "${datasets[@]}"; do
     1 \
     'rse_type=DISK&cms_type=real\tier=3\tier=0' \
     --grouping 'ALL' \
-    --lifetime 1300000 \
+    --lifetime 2592000 \
     --ask-approval \
     --activity "User AutoApprove" \
     --comment "L1-Scouting 2026 commissioning"
